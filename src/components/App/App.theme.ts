d3a49cc23669e9167e6bdc6911239b37bfaa3d58
jfeldman24 <jfeldman@stevens.edu>
2019-09-08 20:06:47 -0400

    typescript

--- src/components/App/App.theme.ts
+++ src/components/App/App.theme.ts
@@ -2,10 +2,10 @@ import { createMuiTheme } from '@material-ui/core';
 
 export const theme = createMuiTheme({
     palette: {
-      primary: '#4059AD',
-      secondary: '#6B9AC4',
+        primary: '#4059AD',
+        secondary: '#6B9AC4',
     },
     status: {
-      danger: '#EE6352',
+        danger: '#EE6352',
     },
-  });
+});
